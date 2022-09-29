1dea6c0def76729d7f777cb3801a7c0dc018cd13
Anas-Nevrekar <112456172+Anas-Nevrekar@users.noreply.github.com>
2022-09-29 14:46:06 +0530

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,16 @@
+var randomNumber = Math.floor(Math.random()*3)+1;
+// alert(randomNumber);
+document.querySelectorAll("img")[1].src = "image"+randomNumber+".png";
+// document.querySelector("h1").innerHTML="Bye";
+if(randomNumber===1)
+{
+    document.querySelector("h1").innerHTML="You won";
+}
+else if(randomNumber===3)
+{
+    document.querySelector("h1").innerHTML="You lose";
+}
+else
+{
+    document.querySelector("h1").innerHTML="Withdraw";
+}
